593f92578c128c9c74190bb530420605dc8c4703
Khushali Trivedi <trivedikhushali47@gmail.com>
2023-06-30 16:08:04 +0530

    files updated

--- controllers/ondc.controller.js
+++ controllers/ondc.controller.js
@@ -365,21 +365,30 @@ class adminOndcController {
     }
 
 
-    // Activate ONDC Store
-    async activate_ondc_store(req, res) {
+    // Active Status ONDC Store
+    async active_status_ondc_store(req, res) {
         try {
             if (!req.params.id) {
                 return res.send({ status: "failure", msg: "Please provide Store ID!" });
             }
-
-            const store = await ondc_store.update({ active: true }, {
-                where: { ondc_store_id: req.params.id },
-            });
-            const store_data = await ondc_store.findOne({ where: { ondc_store_id: req.params.id } });
+            
+            var active = false
+            if (req.query.active) {
+                active = req.query.active;
+            }
+            const store = await ondc_store.update(
+                { active },
+                {
+                    where: { ondc_store_id: req.params.id },
+                }
+            );
             if (store[0]) {
-                return res.json({ status: "success", msg: `${store_data.store_name} Store is Active!` });
+                return res.json({
+                    status: "success",
+                    msg: "Store Status Updated Successfully!",
+                });
             } else {
-                return res.json({ status: "failure", msg: "Please try again later!" });
+                return res.json({ status: "failure", msg: "No Store Found!" });
             }
         } catch (err) {
             console.log(err);
@@ -390,30 +399,6 @@ class adminOndcController {
         }
     }
 
-    // Deactivate ONDC Store
-    async deactivate_ondc_store(req, res) {
-        try {
-            if (!req.params.id) {
-                return res.send({ status: "failure", msg: "Please provide Store ID!" });
-            }
-
-            const store = await ondc_store.update({ active: false }, {
-                where: { ondc_store_id: req.params.id },
-            });
-            const store_data = await ondc_store.findOne({ where: { ondc_store_id: req.params.id } });
-            if (store[0]) {
-                return res.json({ status: "success", msg: `${store_data.store_name} Store is Deactivated!` });
-            } else {
-                return res.json({ status: "failure", msg: "Please try again later!" });
-            }
-        } catch (err) {
-            console.log(err);
-            return res.status(500).json({
-                status: "failure",
-                msg: err,
-            });
-        }
-    }
 
     // Get Franchise
     async get_franchise(req, res) {
